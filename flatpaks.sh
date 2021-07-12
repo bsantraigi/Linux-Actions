0f806ca7c55e4ae1dde188a1cd7cfdfd78768424
Bishal Santra <bsantraigi@gmail.com>
2021-07-12 19:57:20 +0530

    Update flatpaks.sh

--- flatpaks.sh
+++ flatpaks.sh
@@ -1,4 +1,5 @@
 flatpak install flathub com.jetbrains.PyCharm-Professional
+flatpak install flathub com.visualstudio.code
 flatpak install flathub com.spotify.Client
 flatpak install flathub com.microsoft.Teams
 flatpak install flathub com.slack.Slack
